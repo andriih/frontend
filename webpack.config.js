efbe5cdcc57f727925e840b18bd466caf33ab46f
andriih <andrii.hnatyshyn@gmail.com>
2017-10-26 11:54:12 +0300

    html loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const extractPlugin = new ExtractTextPlugin({
     filename: 'main.css'
 });
@@ -30,12 +32,20 @@ module.exports = {
               use: extractPlugin.extract({
                 use: ['css-loader','sass-loader']
               })
+            },
+
+            {
+              test: '/\.html$/',
+              use: ['html-loader']
             }
         ]
       },
       
       plugins: [
-        extractPlugin
+        extractPlugin,
+        new HtmlWebpackPlugin({
+          template: 'src/html/index.html'
+        })
       ]
 
 
